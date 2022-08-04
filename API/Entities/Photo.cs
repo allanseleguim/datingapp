4a405504518a7e6df9b13b9bd189ea5da9427d8d
allanseleguim <seleguimstudio@gmail.com>
2022-08-03 21:42:36 -0300

    session 8

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -6,14 +6,14 @@ namespace API.Entities
     public class Photo
     {
         public int Id { get; set; }
-        public string Url { get; set; }
+        public string? Url { get; set; }
         // public string Description { get; set; }
         // public DateTime DateAdded { get; set; }
         public bool IsMain { get; set; }
-        public string PublicId { get; set; }
+        public int? PublicId { get; set; }
         // public AppUser AppUser { get; set; }
         // public int AppUserId { get; set; }
-        public AppUser AppUser { get; set; }
+        public AppUser? AppUser { get; set; }
         public int AppUserId { get; set; }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -21,6 +21,7 @@ namespace API
             var host = CreateHostBuilder(args).Build();
             using var scope = host.Services.CreateScope();
             var services = scope.ServiceProvider;
+
             try
             {
                 var context = services.GetRequiredService<DataContext>();
